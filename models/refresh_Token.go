78061fc10823ebb37b9db0242c1555771fa1266e
MOHAMMED SHIBILI M <mohammedshbli@gmail.com>
2026-01-31 14:46:59 +0530

    allighment

--- models/refresh_Token.go
+++ models/refresh_Token.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type RefreshToken struct {
-	ID        uint   `gorm:"primaryKey"`
-	UserId    uint   `gorm:"not null"`
-	Token     string `gorm:"not null;unique"`
+	ID        uint      `gorm:"primaryKey"`
+	UserId    uint      `gorm:"not null"`
+	Token     string    `gorm:"not null;unique;varchar(255)"`
 	ExpiredAt time.Time `gorm:"not null"`
-	CreateAt time.Time 
+	CreateAt  time.Time
 	UpdatedAt time.Time
-	DeleteAt time.Time `gorm:"index"`
+	DeleteAt  time.Time `gorm:"index"`
 }
